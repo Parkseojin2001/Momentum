b527eaeb916c2a518a8251144d9b9a7107dcc063
Parkseojin2001 <seojin0510612@gmail.com>
2023-11-28 16:49:05 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked"); // toggle : 있다면 제거 없으면 추가
+function onLoginBtnClick(){
+    const username = loginInput.value;
+    console.log(username);
 }
 
-
-
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
