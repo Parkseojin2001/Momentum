bc1ca680065e64d9b40d4910bd524c7debd2ddaa
Parkseojin2001 <seojin0510612@gmail.com>
2023-11-28 16:06:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    if(h1.className === "active"){
-        h1.className = "";
-    }else{
-        h1.className = "active";
-    }
+    h1.classList.toggle("clicked"); // toggle : 있다면 제거 없으면 추가
 }
 
 
