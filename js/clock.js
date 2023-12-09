ec78fd5b4b29b7f29a208178c11643ee95a7238f
Parkseojin2001 <seojin0510612@gmail.com>
2023-12-10 01:48:30 +0900

    Update .js

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,9 @@
 const clock = document.querySelector("h2#clock");
 
 function getclock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes= String(date.getMinutes()).padStart(2, "0");
-    const seconds= String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
-}
+    clock.innerText = clock.innerText = new Date().toLocaleTimeString("en-US");
+    }
+
 
 getclock();
 setInterval(getclock,1000);
--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const toDo = document.getElementById("todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,6 +9,7 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    toDo.classList.remove(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings();
@@ -15,6 +17,7 @@ function onLoginSubmit(event){
 }
 
 function paintGreetings(){
+    toDo.classList.remove(HIDDEN_CLASSNAME);
     const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}!~`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
